bde88f461d9e0a0d1c3973143cc2712b70cbc42c
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2023-04-06 00:35:57 -0300

    remove tone functions add notification sound

--- src/helpers/helpers.cpp
+++ src/helpers/helpers.cpp
@@ -1,25 +1,6 @@
 #include "helpers.h"
 #include "../defines.h"
 
-void noTone(uint8_t pin, uint8_t channel)
-{
-    ledcDetachPin(pin);
-    ledcWrite(channel, 0);
-}
-
-void tone(uint8_t pin, unsigned int frequency, unsigned long duration, uint8_t channel)
-{
-    if (ledcRead(channel)) {
-        return;
-    }
-    ledcAttachPin(pin, channel);
-    ledcWriteTone(channel, frequency);
-    if (duration) {
-        delay(duration);
-        noTone(pin, channel);
-    }    
-}
-
 void startSound()
 {
   tone(SPEAKER_PIN, 300, 500);
@@ -28,6 +9,23 @@ void startSound()
   delay(500);
   tone(SPEAKER_PIN, 500, 500);
   delay(500);
+}
+
+void notificationSound()
+{
+    tone(SPEAKER_PIN,1319,125);
+    delay(130);
+    tone(SPEAKER_PIN,1568,125);
+    delay(130);
+    tone(SPEAKER_PIN,2637,125);
+    delay(130);
+    tone(SPEAKER_PIN,2093,125);
+    delay(130);
+    tone(SPEAKER_PIN,2349,125);
+    delay(130);
+    tone(SPEAKER_PIN,3136,125);
+    delay(125);
+    noTone(SPEAKER_PIN);
 }
 
 void adjustTimezone(int timezone, int *year, byte *month, byte *day, byte *hour)
--- src/helpers/helpers.h
+++ src/helpers/helpers.h
@@ -3,9 +3,8 @@
 
 #include <Arduino.h>
 
-void noTone(uint8_t pin, uint8_t channel = 0);
-void tone(uint8_t pin, unsigned int frequency, unsigned long duration, uint8_t channel = 0);
 void startSound();
+void notificationSound();
 void adjustTimezone(int timezone, int *year, byte *month, byte *day, byte *hour);
 
 #endif
