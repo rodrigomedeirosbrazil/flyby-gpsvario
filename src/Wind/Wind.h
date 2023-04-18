9ee0aeb56a55dea69632415d75f61a79017a489f
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2023-04-18 14:47:58 -0300

    reset vars

--- src/Wind/Wind.h
+++ src/Wind/Wind.h
@@ -11,8 +11,8 @@ class Wind
         unsigned long getDirection();
     private:
         const unsigned long timeout = 10000;
-        float speed = 7;
-        unsigned long direction = 163;
+        float speed = 0;
+        unsigned long direction = 0;
         unsigned long lastCalculedWind = 0;
         float speeds[16];
         unsigned long times[16];
