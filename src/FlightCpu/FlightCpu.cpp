dcc35a89059aeebe1dbc836db7c2e983ef32523c
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-10-10 12:18:21 -0300

    Refactor FlightCpu to use global instances and remove component members

--- src/FlightCpu/FlightCpu.cpp
+++ src/FlightCpu/FlightCpu.cpp
@@ -1,51 +1,20 @@
 #include "FlightCpu.h"
+#include "../globals.h"
 
 FlightCpu::FlightCpu() {
-    this->barometer = new Barometer();
-    this->variometer = new Variometer();
-    this->beep = new Beep(SPEAKER_PIN);
-    this->gps = new Gps();
-    this->screen = new Screen(this);
-    this->wind = new Wind();
-
-    this->screen->begin();
-}
-
-Variometer* FlightCpu::getVariometer() {
-    return this->variometer;
-}
-
-Gps* FlightCpu::getGps() {
-    return this->gps;
-}
-
-Beep* FlightCpu::getBeep() {
-    return this->beep;
-}
-
-Screen* FlightCpu::getScreen() {
-    return this->screen;
-}
-
-Wind* FlightCpu::getWind() {
-    return this->wind;
-}
-
-Barometer* FlightCpu::getBarometer() {
-    return this->barometer;
 }
 
 void FlightCpu::tick() {
-    if (barometer->isAvailable()) {
-      variometer->tick(barometer->getPressure(), millis());
+    if (barometer.isAvailable()) {
+      variometer.tick(barometer.getPressure(), millis());
     }
     
     if (inFlight) {
-      beep->tick(this->variometer->getVario());
+      beep.tick(variometer.getVario());
     }
 
-    gps->tick();
-    screen->draw();
+    gps.tick();
+    screen.draw();
 
     bySecondTask();
 }
@@ -59,38 +28,38 @@ void FlightCpu::bySecondTask() {
     autoAdjustQNH();
     checkInFlight();
     if (inFlight) {
-      wind->storeSpeed(gps->getHeading(), gps->getSpeed(), millis());
+      wind.storeSpeed(gps.getHeading(), gps.getSpeed(), millis());
     }
 }
 
 void FlightCpu::autoAdjustQNH() {
-  if (! gps->isAvailable() || smallerVdop <= gps->getVdop()) {
+  if (! gps.isAvailable() || smallerVdop <= gps.getVdop()) {
     return;
   }
 
-  smallerVdop = gps->getVdop();
-  variometer->setQnhByAltitude(gps->getAltitude());
+  smallerVdop = gps.getVdop();
+  variometer.setQnhByAltitude(gps.getAltitude());
 }
 
 void FlightCpu::checkInFlight() {
-  if (! inFlight && gps->getSpeed() > TAKEOFF_SPEED && inFlightCounter == 4) {
+  if (! inFlight && gps.getSpeed() > TAKEOFF_SPEED && inFlightCounter == 4) {
     inFlight = true;
     inFlightCounter = 0;
-    startFlightTime = millis(); // TODO: change to timestamp
+    startFlightTime = millis();
     oneUpSound();
-  } else if (! inFlight && gps->getSpeed() > TAKEOFF_SPEED && inFlightCounter < 4) {
+  } else if (! inFlight && gps.getSpeed() > TAKEOFF_SPEED && inFlightCounter < 4) {
     inFlightCounter++;
-  } else if (! inFlight && gps->getSpeed() < TAKEOFF_SPEED && inFlightCounter < 4) {
+  } else if (! inFlight && gps.getSpeed() < TAKEOFF_SPEED && inFlightCounter < 4) {
     inFlightCounter = 0;
-  } else if (inFlight && gps->getSpeed() < TAKEOFF_SPEED && inFlightCounter == 4) {
+  } else if (inFlight && gps.getSpeed() < TAKEOFF_SPEED && inFlightCounter == 4) {
     inFlight = false;
     inFlightCounter = 0;
     fireballSound();
     fireballSound();
     fireballSound();
-  } else if (inFlight && gps->getSpeed() < TAKEOFF_SPEED && inFlightCounter < 4) {
+  } else if (inFlight && gps.getSpeed() < TAKEOFF_SPEED && inFlightCounter < 4) {
     inFlightCounter++;
-  } else if (inFlight && gps->getSpeed() > TAKEOFF_SPEED && inFlightCounter < 4) {
+  } else if (inFlight && gps.getSpeed() > TAKEOFF_SPEED && inFlightCounter < 4) {
     inFlightCounter = 0;
   }
 }
--- src/FlightCpu/FlightCpu.h
+++ src/FlightCpu/FlightCpu.h
@@ -10,7 +10,6 @@
 #include "../Wind/Wind.h"
 #include "../Barometer/Barometer.h"
 
-class Screen;
 class FlightCpu
 {
     public:
@@ -18,24 +17,11 @@ class FlightCpu
         void tick();
         void autoAdjustQNH();
         unsigned long getFlightTime();
-        Variometer* getVariometer();
-        Gps* getGps();
-        Beep* getBeep();
-        Screen* getScreen();
-        Wind* getWind();
-        Barometer* getBarometer();
         inline unsigned long getSmallerVdop() { return smallerVdop; }
 
     private:
         void bySecondTask();
         void checkInFlight();
-
-        Variometer *variometer;
-        Gps *gps;
-        Beep *beep;
-        Screen *screen;
-        Wind *wind;
-        Barometer *barometer;
 
         unsigned long smallerVdop = 9999;
         unsigned long lastTimeBySecondTask = 0;
